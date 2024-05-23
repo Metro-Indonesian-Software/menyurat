ded4d4bb6d938a3cb576790f2a03cb6c52af9fc2
Ade Iqbal <adeiqbal1305@gmail.com>
2024-05-23 09:41:42 +0700

    logo kop surat

--- resources/views/components/buat_surat/kop.blade.php
+++ resources/views/components/buat_surat/kop.blade.php
@@ -1,6 +1,10 @@
 <div class="kop_surat">
     <div class="gambar">
-        <img src="{{ asset("assets/img/profil2.png") }}" alt="Logo perusahaan">
+        @if ($user->logo_url)
+            <img src="{{ asset('storage/' . $user->logo_url) }}" alt="Logo perusahaan">
+        @else
+            <img src="{{ asset('assets/img/profil2.png') }}" alt="Logo perusahaan">
+        @endif
     </div>
     <div class="text-center">
         <p style="font-size: .8em">
@@ -17,7 +21,7 @@
             <span>Email : <span class="text-primary"><u>{{ $user->email }}</u></span></span>
 
             @if ($user->web_url)
-            <span>Website :<span class="text-primary"><u>{{ $user->web_url }}</u></span></span>
+                <span>Website :<span class="text-primary"><u>{{ $user->web_url }}</u></span></span>
             @endif
         </p>
     </div>
